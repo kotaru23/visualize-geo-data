a5c57d59ba9c7bdbf21aa39eedb471ae2e0fd3ee
Asamin <CoffeeTaro@users.noreply.github.com>
2018-05-21 00:49:11 +0900

    [add] コマンドラインツール化

--- visualize-geo.py
+++ visualize-geo.py
@@ -3,18 +3,23 @@ import folium
 import sys
 import pandas as pd
 import click
+from tqdm import tqdm
 
 
 @click.command()
 @click.option("-o", "--output", default="map.html", help="Output HTML file name")
-def main(output):
-    japan_map = folium.Map(location=[35, 135], zoom_start=5, tiles='Stamen Terrain')
+@click.option("-t", "--tile", default="openstreetmap", help="choose map design:\nmapboxbright\ncartodbdark_matter\ncartodbpositron\ncloudmade\nmapbox\nmapboxcontrolroom\n openstreetmap\nstamenterrain\nstamentoner\nstamenwatercolor")
+def main(output, tile):
+    japan_map = folium.Map(location=[35, 135], zoom_start=5, tiles=tile)
     df = pd.read_csv(sys.stdin, header=None)
     geos = df.values.tolist()
-    if len(geos[0]) < 3:
-        return 0
-    for g in geos:
-        folium.Marker(location=[g[0], g[1]], popup=g[2]).add_to(japan_map)
+    for g in tqdm(geos):
+        if len(geos[0]) == 3:
+            # コメントつきの場合
+            folium.Marker(location=[g[0], g[1]], popup=g[2]).add_to(japan_map)
+        if len(geos[0]) == 2:
+            # 緯度・経度のみの場合
+            folium.Marker(location=[g[0], g[1]]).add_to(japan_map)
     japan_map.save(output)
     return None
 
